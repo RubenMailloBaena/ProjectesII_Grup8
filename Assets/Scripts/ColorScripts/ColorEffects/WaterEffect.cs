76806a0fa64a852d72452ee3547a5973018d201d
Silvia-SB <103179506+Silvia-SB@users.noreply.github.com>
2024-04-10 11:30:24 +0200

    ss

--- Assets/Scripts/ColorScripts/ColorEffects/WaterEffect.cs
+++ Assets/Scripts/ColorScripts/ColorEffects/WaterEffect.cs
@@ -14,8 +14,6 @@ public class WaterEffect : IWaterEffect
         this.colorType = colorType; 
     }
 
-
-
     public void InitializeEffect(GameObject target)
     {
         previousColor = target.GetComponent<SpriteRenderer>().color;
@@ -24,9 +22,46 @@ public class WaterEffect : IWaterEffect
         Debug.Log("APLICANDO EFECTO");
     }
 
-    public void ApplyEffect()
+        void OnTriggerEnter2D(Collider2D other)
     {
-        
+        // Verifica si el objeto entrante es el jugador u otro objeto de interés
+        if (other.gameObject.tag == "Player") // Asegúrate de que el jugador tenga asignado el tag "Player"
+        {
+            ApplyEffect(other.gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        // Verifica si el objeto saliente es el jugador u otro objeto de interés
+        if (other.gameObject.tag == "Player")
+        {
+            RemoveEffect(other.gameObject);
+        }
+    }
+
+    public void ApplyEffect(GameObject target)
+    {
+        // Guarda el color original solo si no se ha aplicado previamente el efecto
+        if (previousColor == Color.clear) // Color.clear es solo un ejemplo, necesitarás una mejor lógica para manejar múltiples objetos
+        {
+            previousColor = target.GetComponent<SpriteRenderer>().color;
+        }
+
+        target.GetComponent<SpriteRenderer>().color = effectColor;
+        target.GetComponent<BoxCollider2D>().isTrigger = true;
+
+        // Aquí podrías añadir otros efectos como alterar la gravedad, etc.
+        Debug.Log("Aplicando efecto de agua");
+    }
+
+    public void RemoveEffect(GameObject target)
+    {
+        target.GetComponent<SpriteRenderer>().color = previousColor;
+        target.GetComponent<BoxCollider2D>().isTrigger = false;
+
+        // Restablecer otros efectos aquí
+        Debug.Log("Removiendo efecto de agua");
     }
 
     public ColorType getColorType()
@@ -34,9 +69,4 @@ public class WaterEffect : IWaterEffect
         return colorType;
     }
 
-    public void RemoveEffect(GameObject target)
-    {
-        target.GetComponent<SpriteRenderer>().color = previousColor;
-        target.GetComponent<BoxCollider2D>().isTrigger = false;
-    }
 }

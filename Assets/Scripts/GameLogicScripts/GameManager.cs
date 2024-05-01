e53822d4a2826598f968548f3a16a6b26ede5162
adamblin <abenitez@edu.tecnocampus.cat>
2024-05-01 19:45:35 +0200

    Update GameManager.cs

--- Assets/Scripts/GameLogicScripts/GameManager.cs
+++ Assets/Scripts/GameLogicScripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
         animator = prefabInstance.GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         ManagePauseGame();    
     }

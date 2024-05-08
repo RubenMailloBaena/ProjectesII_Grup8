00356d551119409d3f311e408628bdeeb1413fb4
adamblin <abenitez@edu.tecnocampus.cat>
2024-05-08 20:45:30 +0200

    Push

---
+++ Assets/Scripts/PlayerScripts/HeadAnimations.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadAnimations : MonoBehaviour
+{
+    private static PlayerAnimations instance;
+    public static PlayerAnimations Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindAnyObjectByType<PlayerAnimations>();
+            return instance;
+        }
+    }
+
+    [SerializeField] private Animator animator;
+    private string openName = "Obrir";
+    private string closeName = "Tancar";
+    private string currentState;
+
+
+    public void ChangeAnimation(HeadAnim newAnim)
+    {
+
+        switch (newAnim)
+        {
+            case HeadAnim.Open:
+                ChangeAnimationState(openName); break;
+
+            case HeadAnim.Close:
+                ChangeAnimationState(closeName); break;
+
+            default: throw new System.Exception("error");
+        }
+    }
+    private void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+}
+
+public enum HeadAnim
+{
+    Open,
+    Close
+}

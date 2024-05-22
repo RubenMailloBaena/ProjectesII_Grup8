07eeb62814f674c353d8827721ec112061d5ebdf
RubenMailloBaena <rubenmaillo2003@gmail.com>
2024-05-22 17:54:23 +0200

    hold space in water

--- Assets/Scripts/PlayerScripts/PlayerInputs.cs
+++ Assets/Scripts/PlayerScripts/PlayerInputs.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
 
 public class PlayerInputs : MonoBehaviour
 {
@@ -19,29 +20,36 @@ public class PlayerInputs : MonoBehaviour
     [SerializeField] private KeyCode pauseGame;
     [SerializeField] private KeyCode shootKey;
     [SerializeField] private KeyCode jumpKey;
-
     [SerializeField] private KeyCode swapColor;
 
     //Tecla para disparar
     public event Action onShoot;
-
     //Movimiento
     public event Action onJump;
 
+    private bool inWater;
     //Cambiar Colores
     public event Action onSwapColor;
-
     //Pausar Juego
     public event Action onPauseGame;
 
-
     private void Update()
     {
+        //SHOOT TONGUE
         if (Input.GetKeyDown(shootKey) || Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Mouse0))
             onShoot?.Invoke();
 
-        if(Input.GetKeyDown(jumpKey))
-            onJump?.Invoke();
+        //JUMP
+        if (!inWater)
+        {
+            if(Input.GetKeyDown(jumpKey))
+                onJump?.Invoke();
+        }
+        else
+        {
+            if (Input.GetKey(jumpKey) || Input.GetKeyDown(jumpKey))
+                onJump?.Invoke();
+        }
 
         //COLORS
         if(Input.GetKeyDown(swapColor))
@@ -49,5 +57,19 @@ public class PlayerInputs : MonoBehaviour
 
         if(Input.GetKeyDown(pauseGame))
             onPauseGame?.Invoke();
+    }
+    
+    private void InWater() {
+        inWater = !inWater;
+    }
+
+    private void OnEnable()
+    {
+        WaterEffect.onWater += InWater;
+    }
+
+    private void OnDisable()
+    {
+        WaterEffect.onWater -= InWater;
     }
 }

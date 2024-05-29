05d417079103b6242835dd836e27898da69e7313
TheYuse <107171501+TheYuse@users.noreply.github.com>
2024-05-29 20:09:27 +0200

    .

--- Assets/Scripts/GameLogicScripts/CheckPoint.cs
+++ Assets/Scripts/GameLogicScripts/CheckPoint.cs
@@ -7,21 +7,25 @@ using Unity.VisualScripting;
 public class CheckPoint : MonoBehaviour
 {
     public static event Action<GameObject> onCheckPoint;
+
+    [Header("butttt_0 (1)")]
    
-    
-   
-    
+    private Animator animator;
+    private bool Ceck = false;
     private void Start()
     {
-        
-       
+         animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+     animator.SetBool("isFlyiing", Ceck);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player")) {
-              
-            
+
+            Ceck = true;
             onCheckPoint?.Invoke(gameObject);
             Destroy(GetComponent<Collider2D>());
            
